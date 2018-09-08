c2fbffda3068033d87a9565a8745e04faa799aca
Bryson Kruk <brysonkruk@brysons-air.uwyo.edu>
2018-09-08 15:02:57 -0600

    added initial buttons for blockchain calls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getUsers } from './blockchainService';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      activeUser : '0x895B758229aFF6C0f95146A676bBF579aD7636aa',
+      users : [],
+
+    };
+  }
+  
+  getUsers(){
+    this.setState(() =>({
+      users : getUsers()
+    }), () =>{
+      console.log(this.state.users);
+    });
+  }
+
+  componentDidMount(){
+    this.getUsers();
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +35,31 @@ class App extends Component {
         <p className="App-intro">
           A platform incentivizing water conservation and rehabilitation.
         </p>
+        <section id="title">
+          <h2> Hello, { this.state.activeUser }</h2>
+        </section>
+        <section id="blockchainInteraction">
+          <div>
+            <button>
+                Get Users
+            </button>
+          </div>
+          <div>
+            <input type="text" placeholder="enter address" />
+            <button>Get user tokens</button>
+          </div>
+          <div>
+            <input type="text" placeholder="enter address"  />
+            <button>Transfer ERC 20</button>
+          </div>
+          <div>
+            <input type="text" placeholder="enter address" />
+            <button>Transfer ERC 721</button>
+          </div>
+        </section>
+        <section id="blockchainData">
+          
+        </section>
       </div>
     );
   }
---
+++ src/blockchainService.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+function getUsers(){
+  const users = ['0x895B758229aFF6C0f95146A676bBF579aD7636aa', '0xe6680987f8893F130aa2313acacb2A5eDaa9CC2B', '0xB9ABCD42B1351F62615dc2fF023FE03673122DC4']
+  return users;
+}
+
+export { getUsers };
